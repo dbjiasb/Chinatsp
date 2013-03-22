9f7099bcc4f1abb56b965b531f6e81aea181d808
Bolu Lv <lnsd@wangmatoMac-mini.local>
2013-03-22 14:26:59 +0800

    Initial Commit

---
+++ Chinatsp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Chinatsp
+//
+//  Created by Bolu Lv on 13-3-22.
+//  Copyright (c) 2013年 yuante.Yuan Wen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
